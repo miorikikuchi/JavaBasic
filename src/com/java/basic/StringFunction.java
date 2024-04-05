5df1f2c795d08baba1ce7a7b91fb7468289a5ffc
菊池 美織 <m_kikuchi@sharecrest.com>
2024-04-05 19:01:33 +0900

    切り出し

--- src/com/java/basic/StringFunction.java
+++ src/com/java/basic/StringFunction.java
@@ -35,7 +35,11 @@ public class StringFunction {
 		System.out.println("(5)");
 		System.out.println(String.join(",", letter6));
 		System.out.println(String.join("", letter6));
+		
 		System.out.println("(6)");
+		str2.substring(0,4);
+		str2.substring(4);
+		
 		System.out.println("(7)");
 		System.out.println("(8)");
 		System.out.println("(9)");

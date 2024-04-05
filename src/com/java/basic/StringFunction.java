24739748af51e76cfb6f20ba889e8496c6193478
菊池 美織 <m_kikuchi@sharecrest.com>
2024-04-05 18:33:06 +0900

    文字列の長さ

--- src/com/java/basic/StringFunction.java
+++ src/com/java/basic/StringFunction.java
@@ -3,7 +3,24 @@ package com.java.basic;
 public class StringFunction {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		
+		String str1 = "soba";
+		String str2 ="yakisoba";
+		String str3 ="sobakasu";
+		
+		System.out.println("(1)");
+		System.out.println(str1.length());
+		System.out.println(str2.length());
+		System.out.println(str3.length());
+		
+		System.out.println("(2)");
+		System.out.println("(3)");
+		System.out.println("(4)");
+		System.out.println("(5)");
+		System.out.println("(6)");
+		System.out.println("(7)");
+		System.out.println("(8)");
+		System.out.println("(9)");
 
 	}
 

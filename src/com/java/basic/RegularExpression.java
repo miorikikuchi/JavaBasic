20b9210b785c2d646ecc20c92e787c6edf365605
菊池 美織 <m_kikuchi@sharecrest.com>
2024-04-08 10:49:29 +0900

    正規表現２

--- src/com/java/basic/RegularExpression.java
+++ src/com/java/basic/RegularExpression.java
@@ -4,8 +4,13 @@ public class RegularExpression {
 
 	public static void main(String[] args) {
 		String myNumber = "My cellphone number is 010-1234-5678.";
-		System.out.println(myNumber.replaceAll("[1-9]","x"));
-
+		System.out.println(myNumber.replaceAll("[0-9]","x"));
+		
+		String test ="aaaaaabbbbbcccccccddddefggggggggg012iii";
+		System.out.println(test.replaceAll("[a-z]","0"));
+		System.out.println(test.replaceAll("a*b","0"));
+		System.out.println(test.replaceAll("^.*[0-9]{3}","0"));
+		System.out.println(test.replaceAll("[0-9|a-c]","0"));
 	}
 
 }

1cbda205f92c42b826716f34447ab1f72f599fe2
菊池 美織 <m_kikuchi@sharecrest.com>
2024-04-08 18:40:58 +0900

    getter.setter

--- src/com/java/basic/Calculator.java
+++ src/com/java/basic/Calculator.java
@@ -12,42 +12,61 @@ public class Calculator {
 			this.name =name;
 			countCalcu++;
 		}
+		
 		public Calculator(String name,int x) {
 			this(name);
 			this.result = x;
 		}
+		
 		public Calculator(String name,double x) {
 			this(name);
 			this.result = x;
 		}
+		
 		public static int getCostCalcu() {
 			return countCalcu * COST;
 		}
+		
 		public void init(int a) {
 			this.result = a;
 		}
+		
 		public void add(int a) {
 			this.result += a;
 		}
+		
 		public void sub(int a) {
 			this.result -= a;
 		}
+		
 		public void mult(int a) {
 			this.result *= a;
 		}
+		
 		public void div(int a) {
 			this.result /= a;
 		}
+		
 		public void sqrt() {
 			this.result = Math.sqrt(this.result);
 		}
+		
 		public double getResult() {
 			return this.result;
 		}
+		
 		public String getName() {
 			return this.name;
 		}
-
+		
+		public void setResult(double x) {
+			x = Math.abs(x);
+			this.result =x;
+		}
+		
+		public void setName(String name ) {
+			this.name = name;
+		}
 	}
 
 
--- src/com/java/basic/ObjectMain1.java
+++ src/com/java/basic/ObjectMain1.java
@@ -7,12 +7,8 @@ public class ObjectMain1 {
 		int y = 7;
 		Calculator calcuA = new Calculator("計算機A");
 		Calculator calcuB = new Calculator("計算機B");
-		calcuA.init(3);
-		calcuA.add(2);
-		calcuA.mult(6);
-		calcuB.init(3);
-		calcuB.add(2);
-		calcuB.mult(6);
+		calcuA.setResult(3.0);
+		calcuB.setResult(-3.0);
 		System.out.println("計算機Aの結果:"+calcuA.getResult());
 		System.out.println("計算機Bの結果:"+calcuB.getResult());
 	}

b760ee7927908ab064837c6c6e066dd26072d2aa
菊池 美織 <m_kikuchi@sharecrest.com>
2024-04-10 10:17:04 +0900

    try-catch文

--- src/com/java/basic/JavaException.java
+++ src/com/java/basic/JavaException.java
@@ -5,9 +5,11 @@ public class JavaException {
 	public static void main(String[] args) {
 		String str = null;
 		
-		
+	try {
 		System.out.println(str.length());
-
+	}catch(NullPointerException e) {
+		System.out.println("エラーが発生しました。");
+	}
 	}
 
 }

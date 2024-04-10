5cc5cc98a1ee5f3fe02761a64a4bf818513a5b60
菊池 美織 <m_kikuchi@sharecrest.com>
2024-04-10 16:07:59 +0900

    静的インナークラス

--- src/com/java/basic/InnerClass.java
+++ src/com/java/basic/InnerClass.java
@@ -4,6 +4,7 @@ public class InnerClass {
 
 	public static void main(String[] args) {
 		MobilePhone yamadaPhone = new MobilePhone("山田","080-1111-2222");
+		yamadaPhone.setLanguage("日本語");
 		yamadaPhone.sendMail("高橋","お久しぶりです。\nお元気ですか？");
 
 	}
--- src/com/java/basic/MobilePhone.java
+++ src/com/java/basic/MobilePhone.java
@@ -4,16 +4,23 @@ public class MobilePhone {
 
 	String user;
 	String phoneNo;
+	String language;
 	
 	MobilePhone(String user,String phoneNo) {
 		this.user = user;
 		this.phoneNo = phoneNo;
+		this.language = LanguageSetting.setLanguage(LanguageSetting.DEFAULT_LANGUAGE);
 	}
 	
 	public void sendMail(String sendTo,String message) {
 		MailApplication mailApp= new MailApplication();
 		mailApp.sendMessage(sendTo,message);
 		mailApp.endApp();
+	}
+	
+	public void setLanguage(String language) {
+		this.language = LanguageSetting.setLanguage(language);
+		System.out.println("言語を"+this.language+"に設定しました。\n");
 	}
 	
 	private class MailApplication{
@@ -33,5 +40,18 @@ public class MobilePhone {
 	}
 	}
 	
+	static class LanguageSetting {
+	
+	static final String DEFAULT_LANGUAGE = "英語";
+	static String setLanguage(String language) {
+		if("英語".equals(language) || "English".equals(language)) {
+			return "英語";
+		}else if("日本語".equals(language) || "Japanese".equals(language)) {
+			return "日本語";
+		}else {
+			return DEFAULT_LANGUAGE;
+		}
+	}
+	}
 
 }

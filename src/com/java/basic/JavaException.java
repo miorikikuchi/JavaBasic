6b240e6ff505bf2b78f51449fdf76fb2540df3e4
菊池 美織 <m_kikuchi@sharecrest.com>
2024-04-10 10:24:29 +0900

    複数の例外処理

--- src/com/java/basic/JavaException.java
+++ src/com/java/basic/JavaException.java
@@ -4,12 +4,21 @@ public class JavaException {
 
 	public static void main(String[] args) {
 		String str = null;
-		
-	try {
-		System.out.println(str.length());
-	}catch(NullPointerException e) {
-		System.out.println("エラーが発生しました。");
-	}
+		int a = 10;
+		int b = 2;
+		int result;
+
+		try {
+			result = a / b;
+			System.out.println(result);
+			System.out.println(str.length());
+		} catch (NullPointerException e) {
+			System.out.println("NullPointerExceptionエラーが発生しました。");
+			e.printStackTrace();
+		} catch (ArithmeticException e) {
+			System.out.println("ArithmeticExceptionエラーが発生しました。");
+			e.printStackTrace();
+		}
 	}
 
 }

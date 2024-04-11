e9bde6ae8e6de21ee43cfb28909dc6c2aeb5a42b
菊池 美織 <m_kikuchi@sharecrest.com>
2024-04-11 18:25:21 +0900

    ファイルの作成と削除

--- src/com/java/basic/FileControl.java
+++ src/com/java/basic/FileControl.java
@@ -1,16 +1,20 @@
 package com.java.basic;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FileControl {
 
 	public static void main(String[] args) {
-		File dir = new File("C:\\java-basic\\test1");
-		File[] childdirs = dir.listFiles();
-		for(File childdir : childdirs) {
-			childdir.delete();
+		File dir = new File("C:\\java-basic\\test1\\test2");
+		File file = new File("C:\\\\java-basic\\\\test1\\\\test2\\HelloWorld.txt");
+		
+		try {
+			dir.mkdirs();
+			file.createNewFile();
+		}catch(IOException e) {
+			e.printStackTrace();
 		}
-		dir.delete();
 		System.out.println("成功しました。");
 
 	}

cea4f15657fe3178ae9a6519898b4053764171ad
菊池 美織 <m_kikuchi@sharecrest.com>
2024-04-11 15:06:37 +0900

    ファイルの書き込み

---
+++ src/com/java/basic/FileWriting.java
@@ -0,0 +1,22 @@
+package com.java.basic;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriting {
+
+	public static void main(String[] args) {
+
+		try {
+			FileWriter fw = new FileWriter("C:¥¥java-basic\\test\\HelloWorld.txt");
+			fw.write("Hello.world!!");
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+
+		}
+
+	}
+
+}

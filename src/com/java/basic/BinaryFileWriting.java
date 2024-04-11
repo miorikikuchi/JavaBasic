d877f51ad8334d9a77114a5165d9bb825748b225
菊池 美織 <m_kikuchi@sharecrest.com>
2024-04-11 16:51:26 +0900

    バイナリファイルの読み込み

---
+++ src/com/java/basic/BinaryFileWriting.java
@@ -0,0 +1,24 @@
+package com.java.basic;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class BinaryFileWriting {
+
+	public static void main(String[] args) {
+		try {
+			File binaryFile = new File("C¥¥java-basic\\test\\abc.dat");
+			binaryFile.createNewFile();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		try(FileOutputStream fos = new FileOutputStream("C¥¥java-basic\\test\\abc.dat");){
+			byte[] data = {0b1111111,0b1010101,0b0000111};
+			
+		}
+
+	}
+
+}

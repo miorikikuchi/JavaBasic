a6593394e92ca7cfbdd0f61e07a026cc08b198bb
菊池 美織 <m_kikuchi@sharecrest.com>
2024-04-11 17:26:06 +0900

    バッファリングフィルタ

--- src/com/java/basic/BinaryFileReaing.java
+++ src/com/java/basic/BinaryFileReaing.java
@@ -1,9 +1,21 @@
 package com.java.basic;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class BinaryFileReaing {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		try (FileInputStream fis = new FileInputStream("C:¥¥java-basic\\test\\abc.dat");) {
+			int i = fis.read();
+			while (i != -1) {
+				String b = Integer.toBinaryString(i);
+				System.out.print(b);
+				i = fis.read();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
---
+++ src/com/java/basic/BufferingFilter.java
@@ -0,0 +1,26 @@
+package com.java.basic;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferingFilter {
+
+	public static void main(String[] args) {
+		try(BufferedReader br = new BufferedReader(new FileReader("C:¥¥java-basic\\\\test\\\\HelloWorld.txt"))){
+			String line = br.readLine();
+			int gyoNo = 1;
+			while(line != null) {
+				System.out.println(gyoNo+"行目："+line);
+				line = br.readLine();
+				gyoNo++;
+			}
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+			
+		
+
+	}
+
+}

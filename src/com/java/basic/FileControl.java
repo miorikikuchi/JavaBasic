bc0bd36d6c9742915c0611f2db57c749b4bfc66d
菊池 美織 <m_kikuchi@sharecrest.com>
2024-04-11 18:12:36 +0900

    ディレクトリの削除

--- src/com/java/basic/FileControl.java
+++ src/com/java/basic/FileControl.java
@@ -5,8 +5,12 @@ import java.io.File;
 public class FileControl {
 
 	public static void main(String[] args) {
-		File dir = new File("C:\\java-basic\\test1\\test2");
-		dir.mkdirs();
+		File dir = new File("C:\\java-basic\\test1");
+		File[] childdirs = dir.listFiles();
+		for(File childdir : childdirs) {
+			childdir.delete();
+		}
+		dir.delete();
 		System.out.println("成功しました。");
 
 	}
